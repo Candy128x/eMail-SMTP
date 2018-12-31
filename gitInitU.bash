90bfd83b268c7b6a193416de2444cdcc1bffadc4
Ashishpf <ashish.sondagar@pepperfry.com>
2018-12-31 13:02:09 +0530

    mdfy init </> Mon, 2018-12-31, 13:02:09 IST 365

---
+++ gitInitU.bash
@@ -0,0 +1,13 @@
+echo "pingpong.. Hey Dude ;]"
+
+git init
+
+git checkout -b 9to5
+echo You r on branch 9to5
+
+echo Paste your remote URL:
+read url
+git remote add origin $url
+echo Hey, remote added origin, URL: $url
+
+echo "Ab Code</> push kar :]"
